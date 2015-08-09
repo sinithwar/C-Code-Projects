4510d2c5f3ae615c93815fe91b91880eef114e76
sinithwar <sinithwar@yahoo.com>
2015-08-09 13:28:43 -0700

    Second Commit.

---
+++ AgeDifferentiationSolution/AgeDifferentiation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeDifferentiation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 18)
+            {
+                Console.WriteLine("That's too bad! You will have to wait a couple of years.");
+            }
+            else if (age == 18)
+            {
+                Console.WriteLine("Congrats, you can go get some lung cancer.");
+            }
+            else
+            {
+                Console.WriteLine("Welcome to being old.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MathQuizingSolution/MathQuizing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathQuizing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Start:
+            Random rnd = new Random();
+            double firstNumber = rnd.Next(1,10);
+            double secondNumber = rnd.Next(1,10);
+
+        
+            Console.WriteLine("What is " + firstNumber + " divided by " + secondNumber + "?");
+            double result = firstNumber / secondNumber;
+            double answer = Convert.ToDouble(Console.ReadLine());
+
+            if (answer == result){
+                Console.WriteLine("Congratulations, that's the right.");
+            }else{
+                Console.WriteLine("I'm sorry, that answer is wrong. Let's try this again.");
+            }
+
+
+        goto Start;
+        }
+    }
+}
